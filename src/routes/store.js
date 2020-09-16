ba8bcdeac5a58d8ae446b0a1357cda597459c3a8
Gunil <gunil.na1@gmail.com>
2020-09-16 20:58:24 +0200

    deleted commented codes

--- src/routes/store.js
+++ src/routes/store.js
@@ -1,36 +1,7 @@
-// import { createStore } from "redux";
 import { configureStore, createAction, createReducer } from "@reduxjs/toolkit";
-
-// const ADD = "ADD";
-// const DELETE = "DELETE";
-
-// const addToDo = (text) => {
-//   return {
-//     type: ADD,
-//     text,
-//   };
-// };
-
-// const deleteToDo = (id) => {
-//   return {
-//     type: DELETE,
-//     id: parseInt(id),
-//   };
-// };
 
 const addToDo = createAction("ADD");
 const deleteToDo = createAction("DELETE");
-
-// const reducer = (state = [], action) => {
-//   switch (action.type) {
-//     case addToDo.type:
-//       return [{ text: action.payload, id: Date.now() }, ...state];
-//     case deleteToDo.type:
-//       return state.filter((toDo) => toDo.id !== action.payload);
-//     default:
-//       return state;
-//   }
-// };
 
 const reducer = createReducer([], {
   [addToDo]: (state, action) => {
@@ -40,7 +11,6 @@ const reducer = createReducer([], {
     state.filter((toDo) => toDo.id !== action.payload),
 });
 
-// const store = createStore(reducer);
 const store = configureStore({ reducer });
 
 export const actionCreators = {
